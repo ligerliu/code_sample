1598eb6cb794ad9f3c832647f8947c3ef4d68ec2
ligerliu <ligerliu@gmail.com>
2020-02-11 00:20:45 -0500

    initial documentation

---
+++ docs/build/html/contents-1.py
@@ -0,0 +1,30 @@
+from im_load import load_tiff
+import numpy as np
+import os,glob
+import warnings
+warnings.simplefilter("ignore")
+path = os.path.abspath('../../code')
+os.chdir(path)
+fn = glob.glob('*.tif')[0]
+img = load_tiff(fn).im
+
+from exp_param import ExpConfig
+dexp = ExpConfig(im_x = img.shape[1],
+                 im_y = img.shape[0],
+                 center_x = 715.09,
+                 center_y = 914.08,
+                 wavelength = 1.24,
+                 pixel_size = 172,
+                 detector_distance = 2.03,
+                 )
+
+from data_reduction import proc
+d = proc(img,dexp)
+q = np.linspace(0.001,0.2,300)
+d1d = d.data1d(q)
+
+from data_visual import data_show
+show = data_show(d,dexp)
+show.show2D(vmax = 10000)
+show.show1D(q)
+plt.show()
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../code_sample'))
+import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('../../code'))
 
 
 # -- Project information -----------------------------------------------------
@@ -31,6 +32,14 @@ release = '0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+	'sphinx.ext.autodoc',
+	'sphinx.ext.autosummary',
+	'sphinx.ext.githubpages',
+	'sphinx.ext.intersphinx',
+	'IPython.sphinxext.ipython_directive',
+	'IPython.sphinxext.ipython_console_highlighting',
+	'matplotlib.sphinxext.plot_directive',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,9 +56,10 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
+#html_theme = 'alabaster'
+html_them = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []#['_static']
